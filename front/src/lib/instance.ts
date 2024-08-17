62e07cfe2888c86344b9a632111c3e2398e744ca
Nick MakeAvoy <makeavoy@gmail.com>
2024-08-17 19:44:09 -0400

    feat: curve with data texture

--- front/src/lib/instance.ts
+++ front/src/lib/instance.ts
@@ -65,65 +65,9 @@ export function create(element: HTMLCanvasElement, pointSet: (point: Vector3) =>
 
   const divisor = 1 / array.length;
   {
-    // curve
-    const curve = new three.CatmullRomCurve3([
-      new three.Vector3(0, 10, 0),
-      new three.Vector3(0.4, 8, 0.1),
-      new three.Vector3(1.2, 6, -3),
-      new three.Vector3(0.1, 4, -4),
-      new three.Vector3(-1, 2, -7),
-      new three.Vector3(-0.6, 0, -12),
-    ]);
+    
 
-    const points = curve.getPoints(50);
-    //  geometry.attributes.position.itemSize
-    const size = points.length * 2;
-    const verts: three.Vector3[] = new Array(size);
-    const indices = new Array((points.length - 1) * 6);
-    const norms: three.Vector3[] = new Array(size);
-    const uvs = new Array(size);
-    for (let i = 0; i < points.length; i++) {
-      const v2 = points[i].clone();
-      v2.x -= 1;
-      const c = i * 2;
-      const d = i * 2 + 1;
-
-      verts[c] = points[i];
-      verts[d] = v2;
-      if (i > 0) {
-        // perpendicular
-        const perp = points[i]
-          .clone()
-          .sub(points[i - 1])
-          .normalize()
-          .cross(new three.Vector3(0, 0, -1));
-        perp.y = 0;
-        perp.normalize();
-        // A B
-        // C D
-        const a = c - 2;
-        const b = d - 2;
-        const ii = i * 6;
-        // tri 1 ABC
-        indices[ii] = a;
-        indices[ii + 1] = b;
-        indices[ii + 2] = d;
-        // tri 2 DCB
-        indices[ii + 3] = d;
-        indices[ii + 4] = c;
-        indices[ii + 5] = a;
-
-        norms[c] = new three.Vector3();
-        norms[d] = perp;
-        const u = i / points.length;
-        uvs[c] = new three.Vector2(u, 0);
-        uvs[d] = new three.Vector2(u, 1);
-      }
-    }
-    norms[norms.length - 2] = norms[norms.length - 3];
-    norms[norms.length - 1] = norms[norms.length - 2];
-    uvs[uvs.length - 2] = uvs[uvs.length - 3];
-    uvs[uvs.length - 1] = uvs[uvs.length - 2];
+    
 
     const geometry = new three.BufferGeometry().setFromPoints(verts);
     geometry.setIndex(indices);
@@ -221,15 +165,75 @@ export function create(element: HTMLCanvasElement, pointSet: (point: Vector3) =>
 
     // Create the final object to add to the scene
     const mesh = new three.Mesh(geometry, ribbonMaterial);
-    ribbon = mesh;
-
     // YELLOW
+    ribbon = mesh;
 
     const simpleGeometry = new three.BufferGeometry().setFromPoints(points);
     const simpleMaterial = new three.LineBasicMaterial({ color: 0xff0000, linewidth: 1 });
     const simple = new three.Line(simpleGeometry, simpleMaterial);
     root.add(simple);
     root.add(mesh);
+  }
+
+  function buildCurve(): {verts: three.Vector3[], indices: number[], norms: three.Vector3[], uvs: three.Vector2[]} {
+// curve
+    const curve = new three.CatmullRomCurve3([
+      new three.Vector3(0, 10, 0),
+      new three.Vector3(0.4, 8, 0.1),
+      new three.Vector3(1.2, 6, -3),
+      new three.Vector3(0.1, 4, -4),
+      new three.Vector3(-1, 2, -7),
+      new three.Vector3(-0.6, 0, -12),
+    ]);
+    const points = curve.getPoints(50);
+    // YELLOW
+const size = points.length * 2;
+    const verts: three.Vector3[] = new Array(size);
+    const indices = new Array((points.length - 1) * 6);
+    const norms: three.Vector3[] = new Array(size);
+    const uvs = new Array(size);
+    for (let i = 0; i < points.length; i++) {
+      const v2 = points[i].clone();
+      v2.x -= 1;
+      const c = i * 2;
+      const d = i * 2 + 1;
+
+      verts[c] = points[i];
+      verts[d] = v2;
+      if (i > 0) {
+        // perpendicular
+        const perp = points[i]
+          .clone()
+          .sub(points[i - 1])
+          .normalize()
+          .cross(new three.Vector3(0, 0, -1));
+        perp.y = 0;
+        perp.normalize();
+        // A B
+        // C D
+        const a = c - 2;
+        const b = d - 2;
+        const ii = i * 6;
+        // tri 1 ABC
+        indices[ii] = a;
+        indices[ii + 1] = b;
+        indices[ii + 2] = d;
+        // tri 2 DCB
+        indices[ii + 3] = d;
+        indices[ii + 4] = c;
+        indices[ii + 5] = a;
+
+        norms[c] = new three.Vector3();
+        norms[d] = perp;
+        const u = i / points.length;
+        uvs[c] = new three.Vector2(u, 0);
+        uvs[d] = new three.Vector2(u, 1);
+      }
+    }
+    norms[norms.length - 2] = norms[norms.length - 3];
+    norms[norms.length - 1] = norms[norms.length - 2];
+    uvs[uvs.length - 2] = uvs[uvs.length - 3];
+    uvs[uvs.length - 1] = uvs[uvs.length - 2];
   }
 
   function animate() {
