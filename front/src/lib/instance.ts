91a40576bcb62231672b8265d115ad2c0de8cea5
Nick MakeAvoy <makeavoy@gmail.com>
2024-10-21 01:05:02 -0400

    fix: better billboarding

--- front/src/lib/instance.ts
+++ front/src/lib/instance.ts
@@ -3,18 +3,30 @@ import * as three from "three";
 import { OrbitControls, type Vector } from "three/examples/jsm/Addons.js";
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-export function create(element: HTMLCanvasElement, pointSet: (point: Vector3) => void): void {
+export function create(
+  element: HTMLCanvasElement,
+  pointSet: (point: Vector3) => void,
+): void {
   console.log("create");
 
   let WARP = false;
   let BILLBOARD = false;
   let SNAP = true;
+  const MAX_DATA = 16384;
+  let dataHeight = 1;
+  let valueArray: Uint8ClampedArray;
+  let geometry: three.BufferGeometry;
 
   const scene = new three.Scene();
   const root = new three.Group();
   root.position.set(0, -5, 0);
   scene.add(root);
-  const cam = new three.PerspectiveCamera(75, element.clientWidth / element.clientHeight, 0.1, 1000);
+  const cam = new three.PerspectiveCamera(
+    75,
+    element.clientWidth / element.clientHeight,
+    0.1,
+    1000,
+  );
 
   const renderer = new three.WebGLRenderer({ canvas: element, alpha: false });
   renderer.setSize(element.clientWidth, element.clientHeight);
@@ -59,21 +71,41 @@ export function create(element: HTMLCanvasElement, pointSet: (point: Vector3) =>
 
   cam.position.z = 20;
 
+  const curve = new three.CatmullRomCurve3([
+    new three.Vector3(0, 20, -10),
+    new three.Vector3(0, 10, 0),
+    new three.Vector3(0.4, 8, 0.1),
+    new three.Vector3(1.2, 6, -3),
+    new three.Vector3(0.1, 4, -4),
+    // new three.Vector3(-6, 2, 7),
+    // new three.Vector3(-0.6, 0, -12),
+  ]);
+  const points = curve.getPoints(10);
+  const p = new Float32Array(points.length * 3);
+  for (let i = 0; i < points.length; i++) {
+    p[i * 3] = points[i].x;
+    p[i * 3 + 1] = points[i].y;
+    p[i * 3 + 2] = points[i].z;
+  }
+
   /// BLUE
 
-  const { texture, array } = createTexture();
+  const { texture, array } = createTestTexture();
 
+  valueArray = array;
   const divisor = 1 / array.length;
   {
-    
+    const { verts, indices, norms, uvs } = buildCurve(p);
 
-    
+    const geometry = new three.BufferGeometry();
 
-    const geometry = new three.BufferGeometry().setFromPoints(verts);
+    geometry.setAttribute(
+      "position",
+      new three.Float32BufferAttribute(verts, 3),
+    );
+
+    // geometry.setAttribute("index", new three.Uint16BufferAttribute(indices, 1));
     geometry.setIndex(indices);
-
-    const vs = verts.flatMap((v) => [v.x, v.y, v.z]);
-    geometry.setAttribute("position", new three.Float32BufferAttribute(vs, 3));
 
     const ns = norms.flatMap((v) => [v.x, v.y, v.z]);
     geometry.setAttribute("normal", new three.Float32BufferAttribute(ns, 3));
@@ -95,32 +127,34 @@ export function create(element: HTMLCanvasElement, pointSet: (point: Vector3) =>
         },
         highlight: { value: 0.5 },
         divisor: { value: divisor / 2 },
-        billboard: { value: BILLBOARD },
+        dataHeight: { value: dataHeight },
+        billboard: { value: true },
+        scale: { value: 4 },
       },
       vertexShader: `
-        uniform vec3 color;
-        varying vec3 vColor;
-        varying vec4 vPos;
-        // attribute vec3 normal;
-        varying vec4 vNormal;
-        varying vec2 vUv;
-        uniform bool billboard;
+uniform vec3 color;
+varying vec3 vColor;
+varying vec4 vPos;
+varying vec4 vNormal;
+varying vec2 vUv;
+uniform bool billboard;
+uniform float scale;
 
+void main() {
+    vColor = color;
+    vUv = uv;
+    if(billboard) {
+        vec3 cameraRight=vec3(viewMatrix[0].x,viewMatrix[1].x,viewMatrix[2].x);
+        // vec3 cameraUp=vec3(viewMatrix[0].y,viewMatrix[1].y,viewMatrix[2].y);
+        // vec3 vPosition=(cameraRight*position.x)+(cameraUp*position.y);
+        vPos = projectionMatrix * modelViewMatrix * vec4(position + normal*cameraRight*scale, 1.0);
+    } else {
+        vPos = projectionMatrix * modelViewMatrix * vec4(position + vec3(1.,0.,0.) * scale, 1.0);
+    }
 
-        void main() {
-            vColor = color;
-            vUv = uv;
-            vPos = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-            // vPos+=  3.*vec4(normal.x,normal.y, normal.z, 1.0) * projectionMatrix ;
-            vNormal =  vec4(normal, 1.0);
-             
-
-
-            if (billboard) {
-              vPos+=  6.*vec4(normal, 1.0) *modelMatrix  ;
-            }
-            gl_Position = vPos;
-        }
+    vNormal = vec4(normal, 1.0);
+    gl_Position = vPos;
+}
       `,
       fragmentShader: `
         varying vec3 vColor;
@@ -128,36 +162,31 @@ export function create(element: HTMLCanvasElement, pointSet: (point: Vector3) =>
         varying vec4 vNormal;
         varying vec2 vUv;
         uniform mediump usampler2D tex;
-        uniform usampler2D ramp;
+        uniform mediump usampler2D ramp;
         uniform float highlight;
         uniform float divisor;
+        uniform float dataHeight;
+
         void main() {
-          // vec2 vCoords = vPos.xy;
-          // vCoords /= vPos.w;
-          // vCoords = vCoords * 0.5 + 0.5;
-          // vec2 uv = fract( vCoords * 10.0 );
-
           // if between highlight minus 0.1 and highlight plus 0.1
-
-          uint p = texture(tex, vUv).r;
+          vec2 uv2 = vec2(vUv.x*dataHeight,vUv.y);
+          uint p = texture(tex, uv2).r;
           float f = float(p) / 255.0;
           uvec4 c = texture(ramp, vec2(f, 0.5));
           if (f < vUv.y) {
             discard;
           } else {
-           
-          vec4 final = vec4(float(c.r) / 255.0, float(c.g) / 255.0, float(c.b) / 255.0, 1.0);
-          float v1= highlight - 7.*divisor;
-          float v2= highlight -divisor;
-          float v3= highlight +divisor;
-        float v4= highlight + 7.*divisor;
-          if (vUv.x > v1 && vUv.x < v2 || vUv.x > v3 && vUv.x < v4) {
-           final /= 4.0;
+            vec4 final = vec4(float(c.r) / 255.0, float(c.g) / 255.0, float(c.b) / 255.0, 1.0);
+            float v1= highlight - 7.*divisor;
+            float v2= highlight -divisor;
+            float v3= highlight +divisor;
+            float v4= highlight + 7.*divisor;
+            if (vUv.x > v1 && vUv.x < v2 || vUv.x > v3 && vUv.x < v4) {
+             final /= 4.0;
+            }
+            gl_FragColor  = final;
+          }
 
-          }
-          gl_FragColor  = final;
-          }
-          
         }
       `,
       side: three.DoubleSide,
@@ -169,71 +198,199 @@ export function create(element: HTMLCanvasElement, pointSet: (point: Vector3) =>
     ribbon = mesh;
 
     const simpleGeometry = new three.BufferGeometry().setFromPoints(points);
-    const simpleMaterial = new three.LineBasicMaterial({ color: 0xff0000, linewidth: 1 });
+    const simpleMaterial = new three.LineBasicMaterial({
+      color: 0xff0000,
+      linewidth: 1,
+    });
     const simple = new three.Line(simpleGeometry, simpleMaterial);
     root.add(simple);
     root.add(mesh);
   }
 
-  function buildCurve(): {verts: three.Vector3[], indices: number[], norms: three.Vector3[], uvs: three.Vector2[]} {
-// curve
-    const curve = new three.CatmullRomCurve3([
-      new three.Vector3(0, 10, 0),
-      new three.Vector3(0.4, 8, 0.1),
-      new three.Vector3(1.2, 6, -3),
-      new three.Vector3(0.1, 4, -4),
-      new three.Vector3(-1, 2, -7),
-      new three.Vector3(-0.6, 0, -12),
-    ]);
-    const points = curve.getPoints(50);
-    // YELLOW
-const size = points.length * 2;
-    const verts: three.Vector3[] = new Array(size);
-    const indices = new Array((points.length - 1) * 6);
-    const norms: three.Vector3[] = new Array(size);
-    const uvs = new Array(size);
-    for (let i = 0; i < points.length; i++) {
-      const v2 = points[i].clone();
-      v2.x -= 1;
-      const c = i * 2;
-      const d = i * 2 + 1;
+  interface Curve {
+    verts: Float32Array;
+    indices: number[];
+    norms: three.Vector3[];
+    uvs: three.Vector2[];
+  }
 
-      verts[c] = points[i];
-      verts[d] = v2;
+  function buildCurve(
+    points: Float32Array,
+    trajectory?: three.Vector3[],
+  ): Curve {
+    const size = points.length * 2;
+    const verts: Float32Array = new Float32Array(size);
+    const indices = new Array(size - 6);
+    const norms: three.Vector3[] = new Array(size / 3);
+    const uvs = new Array(size / 3);
+
+    // TODO temporary
+    trajectory = undefined;
+
+    uvs[0] = new three.Vector2(0, 0);
+    uvs[1] = new three.Vector2(0, 1);
+    for (let i = 0; i < points.length; i += 3) {
+      let v2x = points[i];
+      let v2y = points[i + 1];
+      let v2z = points[i + 2];
+
+      const vi = i / 3;
+
+      const vc = i * 2;
+      const vd = i * 2 + 3;
+
+      const c = vi * 2;
+      const d = vi * 2 + 1;
+
+      verts[vc] = points[i];
+      verts[vc + 1] = points[i + 1];
+      verts[vc + 2] = points[i + 2];
+
       if (i > 0) {
         // perpendicular
-        const perp = points[i]
-          .clone()
-          .sub(points[i - 1])
-          .normalize()
-          .cross(new three.Vector3(0, 0, -1));
-        perp.y = 0;
-        perp.normalize();
+        // let perp;
+
+        // const vec = new three.Vector3(
+        //   points[i] - points[i - 3],
+        //   points[i + 1] - points[i - 2],
+        //   points[i + 2] - points[i - 1],
+        // ).normalize();
+        // perp = vec.clone().cross(new three.Vector3(0, 0, -1));
+        // perp = vec.clone();
+        // perp.normalize();
+        //
+        const ii = i * 6;
         // A B
         // C D
         const a = c - 2;
         const b = d - 2;
-        const ii = i * 6;
+        // const ii = i * 6;
         // tri 1 ABC
         indices[ii] = a;
         indices[ii + 1] = b;
         indices[ii + 2] = d;
-        // tri 2 DCB
+        // tri 2 DCB  // dca
         indices[ii + 3] = d;
         indices[ii + 4] = c;
         indices[ii + 5] = a;
 
         norms[c] = new three.Vector3();
-        norms[d] = perp;
+        // norms[d] = perp;
+        norms[d] = new three.Vector3(1, 1, 1);
         const u = i / points.length;
         uvs[c] = new three.Vector2(u, 0);
         uvs[d] = new three.Vector2(u, 1);
       }
+
+      verts[vd] = v2x;
+      verts[vd + 1] = v2y;
+      verts[vd + 2] = v2z;
     }
+    norms[0] = norms[2];
+    norms[1] = norms[3];
     norms[norms.length - 2] = norms[norms.length - 3];
     norms[norms.length - 1] = norms[norms.length - 2];
-    uvs[uvs.length - 2] = uvs[uvs.length - 3];
-    uvs[uvs.length - 1] = uvs[uvs.length - 2];
+    uvs[uvs.length - 2] = new three.Vector2(1, 0);
+    uvs[uvs.length - 1] = new three.Vector2(1, 1);
+    return { verts, indices, norms, uvs };
+  }
+
+  function rebuild(
+    points: Float32Array,
+    values: Float32Array,
+    min: number,
+    max: number,
+    trajectory?: three.Vector3[],
+  ): void {
+    const { verts, indices, norms, uvs } = buildCurve(points);
+    remakeTexture(values, min, max);
+    geometry.setAttribute(
+      "position",
+      new three.Float32BufferAttribute(verts, 3),
+    );
+    geometry.setIndex(indices);
+    geometry.setAttribute(
+      "normal",
+      new three.Float32BufferAttribute(
+        norms.flatMap((v) => [v.x, v.y, v.z]),
+        3,
+      ),
+    );
+    geometry.setAttribute(
+      "uv",
+      new three.Float32BufferAttribute(
+        uvs.flatMap((v) => [v.x, v.y]),
+        2,
+      ),
+    );
+    // setBoundDirty(true);
+  }
+
+  function remakeTexture(values: Float32Array, min: number, max: number): void {
+    const uintSize = values.length;
+    // var oldBuffer = new ArrayBuffer(20);
+
+    // var newBuffer = new ArrayBuffer(40);
+    // new Uint8Array(newBuffer).set(oldBuffer);
+    // this.valueBuffer.set()
+
+    // const img = this.texture.image;
+    const data = new Uint8ClampedArray(uintSize);
+    for (let j = 0; j < uintSize; j++) {
+      const n = Math.min(Math.max(values[j], min), max) / max;
+      data[j] = Math.floor(n * 255);
+    }
+
+    // this.valueBuffer.subarray(0, data.length);
+    // this.valueBuffer.buffer.
+    valueArray.set(data, 0);
+    dataHeight = uintSize / MAX_DATA;
+    // this.texture.
+    // this.texture.image = { data, width: uintSize, height: 1 };
+    // const texture = new three.DataTexture(uintArray, uintSize, 1, three.RedIntegerFormat, three.UnsignedByteType);
+    // texture.internalFormat = 'R8UI';
+    // this.texture.needsUpdate = true;
+  }
+
+  function createTestTexture(): {
+    texture: three.DataTexture;
+    array: Uint8ClampedArray;
+  } {
+    const uintSize = 2000;
+    // by using an arraybuffer instead of directly creating the TypeArray we can specify the memory address is resizable
+    const uintArray = new Uint8ClampedArray(uintSize);
+
+    dataHeight = uintSize / MAX_DATA;
+    alternatingArray(uintArray);
+    const texture = new three.DataTexture(
+      uintArray,
+      uintSize,
+      1,
+      three.RedIntegerFormat,
+      three.UnsignedByteType,
+    );
+    // texture.internalFormat = "RGBA8UI";
+    texture.internalFormat = "R8UI";
+    // texture.magFilter = three.NearestFilter;
+    texture.needsUpdate = true;
+
+    return { texture, array: uintArray };
+  }
+
+  // function testWarpArray(array: Uint8ClampedArray): void {
+  //   let r = 0;
+  //   for (let j = 0; j < array.length; j++) {
+  //     r += Math.random() * 0.1;
+  //     let d = Math.abs(Math.cos(r)) * 127 + 64;
+  //     d += Math.random() * 64;
+  //     array[j] = d;
+  //   }
+  // }
+
+  function alternatingArray(array: Uint8ClampedArray): void {
+    for (let j = 0; j < array.length; j++) {
+      array[j] = (j % 2) * 200 + 50;
+    }
   }
 
   function animate() {
@@ -242,10 +399,10 @@ const size = points.length * 2;
     // root.rotation.y += 0.01;
     // array.set
 
-    if (WARP) {
-      warpArray(array);
-      texture.needsUpdate = true;
-    }
+    // if (WARP) {
+    //   warpArray(array);
+    //   texture.needsUpdate = true;
+    // }
 
     controls.update();
 
@@ -259,7 +416,9 @@ const size = points.length * 2;
       if (inter.uv) {
         let index;
         if (SNAP) {
-          index = -divisor / 2 + Math.floor((inter.uv.x + divisor) * array.length) / array.length;
+          index =
+            -divisor / 2 +
+            Math.floor((inter.uv.x + divisor) * array.length) / array.length;
         } else {
           index = inter.uv.x;
         }
@@ -300,7 +459,13 @@ function createTexture(): { texture: three.DataTexture; array: Uint8Array } {
   const uintSize = 1024;
   const uintArray = new Uint8Array(uintSize);
   warpArray(uintArray);
-  const texture = new three.DataTexture(uintArray, uintSize, 1, three.RedIntegerFormat, three.UnsignedByteType);
+  const texture = new three.DataTexture(
+    uintArray,
+    uintSize,
+    1,
+    three.RedIntegerFormat,
+    three.UnsignedByteType,
+  );
   // texture.internalFormat = "RGBA8UI";
   texture.internalFormat = "R8UI";
   // texture.magFilter = three.NearestFilter;
@@ -328,7 +493,13 @@ function createColorRamp(array: number[]): three.Texture {
     uintArray[j * 4 + 2] = color & 255;
     uintArray[j * 4 + 3] = 255;
   }
-  const t = new three.DataTexture(uintArray, array.length, 1, three.RGBAIntegerFormat, three.UnsignedByteType);
+  const t = new three.DataTexture(
+    uintArray,
+    array.length,
+    1,
+    three.RGBAIntegerFormat,
+    three.UnsignedByteType,
+  );
   t.internalFormat = "RGBA8UI";
   t.magFilter = three.NearestFilter;
   t.needsUpdate = true;
