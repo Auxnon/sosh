ab95069b4f423b8039f2b9578f7893ad7a901051
Nick MakeAvoy <makeavoy@gmail.com>
2025-02-16 01:44:36 -0500

    feat: rust nif for universe resource with ent array

--- lib/chat_test/universe.ex
+++ lib/chat_test/universe.ex
@@ -3,5 +3,8 @@ defmodule ChatTest.Universe do
 
   # When your NIF is loaded, it will override this function.
   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def bigbang(), do: :erlang.nif_error(:nif_not_loaded)
+  def new_ent(_universe ), do: :erlang.nif_error(:nif_not_loaded)
+  def move_ent(_universe, _id, _pos), do: :erlang.nif_error(:nif_not_loaded)
+  def get_state(_universe), do: :erlang.nif_error(:nif_not_loaded)
 end
-
--- native/universe/src/lib.rs
+++ native/universe/src/lib.rs
@@ -1,24 +1,54 @@
-use entity::Entity;
+use std::{ops::DerefMut, sync::{Mutex, RwLock}};
+
+use entity::{Entity, Vec3};
 use hashbrown::HashMap;
-use rustler::{NifResult, ResourceArc};
+use rustler::{env, Decoder, Encoder, Env, Error, NifResult, Resource, ResourceArc, Term};
 use uuid::Uuid;
 
 mod entity;
 
-
 struct Universe {
-    entities: HashMap<Uuid, Entity>,
+    pub entities: HashMap<Uuid, Entity>,
 }
 
 impl Universe {
     fn new() -> Self {
         Self {
-            entities: Mutex::new(HashMap::default()),
+            entities: HashMap::default(),
         }
+    }
+    fn get_state<'a>(&self, env: Env<'a>) -> Vec<Term<'a>> {
+        self.entities
+            .values()
+            .map(|e| e.into_term(env))
+            .collect()
     }
 }
 
-type ArcUni = ResourceArc<Universe>;
+
+struct UniLock(RwLock<Universe>);
+impl std::ops::Deref for UniLock {
+    type Target = RwLock<Universe>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for UniLock {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+        // let a=self.deref_mut();
+    }
+}
+impl Resource for UniLock {}
+type ArcUni = ResourceArc<UniLock>;
+
+fn load(env: Env, _term: Term) -> bool {
+    // let _a=env.register::<UL>().is_ok();
+    let _b=env.register::<UniLock>().is_ok();
+    true
+}
 
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
@@ -26,25 +56,85 @@ fn add(a: i64, b: i64) -> i64 {
 }
 
 #[rustler::nif]
-fn bigbang() -> NifResult<ArcUni> {
+fn bigbang() -> ArcUni {
     let u = Universe::new();
-    Ok(ResourceArc::new(u))
+    ResourceArc::new(UniLock(RwLock::new(u)))
+    // ResourceArc::new(UL{ mutex: Mutex::new(u)})
+}
+// #[rustler::nif]
+// fn bigbang() -> Universe {
+//     Universe::new()
+// }
+
+// #[rustler::nif]
+// fn update(state: ArcUni, value: f64) -> ArcUni {
+//     // state.
+//     // let mut data = state.data.lock().unwrap();
+//     // data.push(value);
+//     state
+// }
+
+// impl Decoder for Uuid{
+//     decode
+// }
+// fn none_err<T>(option: Option<T>)-> NifResult<>
+
+// return rust uuid as an elixir binary
+#[rustler::nif]
+fn new_ent(env: Env, state: ArcUni) -> NifResult<Term> {
+    match state.write() {
+        Ok(mut u) => {
+            let e = Entity::new();
+            let id = e.get_id();
+            u.entities.insert(e.get_id(), e);
+            Ok(id.as_bytes().encode(env))
+        }
+        Err(_) => Err(Error::Term(Box::new("Universe lock poisoned"))),
+    }
 }
 
 #[rustler::nif]
-fn update(state:ArcUni, value: f64) -> ArcUni{
-    // state.
-    // let mut data = state.data.lock().unwrap();
-    data.push(value);
-    state
+fn move_ent(state: ArcUni, id: Term, pos: Vec3) -> NifResult<()> {
+    // let n = uuid::Uuid::fr
+    //
+    // if let Ok(bytes) =id.decode::<rustler::Binary>() {
+    //     return Uuid::from_slice(bytes.as_slice())
+    //         .map_err(|_| Error::Term(Box::new("Invalid UUID bytes")));
+    // }
+
+    state.write().unwrap().entities.get_mut(&Uuid::new_v4());
+
+    match (id.decode_as_binary(), state.write()) {
+        (Ok(b), Ok(mut u)) => {
+            let id = Uuid::from_slice(b.as_slice())
+                .map_err(|_| Error::Term(Box::new("Invalid UUID bytes")))?;
+            match u.entities.get_mut(&id) {
+                Some(e) => {
+                    e.nav(pos);
+                    Ok(())
+                }
+                None => Err(Error::Term(Box::new("Entity missing"))),
+            }
+        }
+        (Err(e), _) => Err(e),
+        (_, Err(_)) => Err(Error::Term(Box::new("Universe lock poisoned"))),
+    }
 }
 
 #[rustler::nif]
-fn init_rust_state() -> NifResult<ResourceArc<StateResource>> {
-    let state = StateResource {
-        data: Mutex::new(Vec::new()),
-    };
-    Ok(ResourceArc::new(state))
+fn get_state(env: Env, state: ArcUni) -> NifResult<Vec<Term>> {
+    match state.read() {
+        Ok(u) => Ok(u.get_state(env)),
+        Err(_) => Err(Error::Term(Box::new("Universe lock poisoned"))),
+    }
 }
 
-rustler::init!("Elixir.ChatTest.Universe");
+// #[rustler::nif]
+// fn init_rust_state() -> NifResult<ResourceArc<StateResource>> {
+//     let state = StateResource {
+//         data: Mutex::new(Vec::new()),
+//     };
+//     Ok(ResourceArc::new(state))
+// }
+
+rustler::init!("Elixir.ChatTest.Universe", load = load);
