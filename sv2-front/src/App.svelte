874e6436227b76248684d6acaa78a5da1a8648cc
Nick MakeAvoy <makeavoy@gmail.com>
2025-12-15 08:16:25 -0500

    wip: new new svelte variation for front

---
+++ sv2-front/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import ThreeCanvas from './lib/ThreeCanvas.svelte';
+</script>
+
+<div class="app">
+  <ThreeCanvas />
+</div>
+
+<style>
+  .app {
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+</style>
